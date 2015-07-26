36849333518c9c845b8797fc13e358ea8be2e986
flysaiah <mayeis01@yahoo.com>
2015-07-25 19:19:18 -0500

    Commented out demo code

--- playcraftlabs/playcraft/projects/yourgame/js/gamescene.js
+++ playcraftlabs/playcraft/projects/yourgame/js/gamescene.js
@@ -12,26 +12,29 @@ GameScene = pc.Scene.extend('GameScene',
         {
             this._super();
 
-            this.boxes = [];
+            // this.boxes = [];
 
             //-----------------------------------------------------------------------------
             // game layer
             //-----------------------------------------------------------------------------
                            //this refers to the GameScene
-            this.gameLayer = this.addLayer(new pc.EntityLayer('game layer', 10000, 10000));
+            // this.gameLayer = this.addLayer(new pc.EntityLayer('game layer', 10000, 10000));
 
             // all we need is the render and effects systems
-            this.gameLayer.addSystem(new pc.systems.Render());
-            this.gameLayer.addSystem(new pc.systems.Effects());
+            // this.gameLayer.addSystem(new pc.systems.Render());
+            // this.gameLayer.addSystem(new pc.systems.Effects());
 
-            for (var i = 0; i < 3; i++)
+            // this.mainChar = pc.Entity.create(this.gameLayer);
+            // this.mainChar.addComponent(pc.components.Spatial.create({x:200, y:200});
+
+            /*for (var i = 0; i < 3; i++)
             {
                 var box = pc.Entity.create(this.gameLayer);
                 box.addComponent(pc.components.Spatial.create({ x:200 + (i * 100), y:200, w:75, h:75 }));
                 box.addComponent(pc.components.Rect.create({ color:[ pc.Math.rand(0, 255), pc.Math.rand(0, 255), pc.Math.rand(0, 255) ] }));
 
                 this.boxes.push(box);
-            }
+            }*/
 
             // bind some keys/clicks/touches to access the menu
             pc.device.input.bindAction(this, 'menu', 'ENTER');
